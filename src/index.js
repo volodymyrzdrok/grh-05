14c13194e981a5f9eafd3c42ee120c37455bd15d
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-01-31 19:30:47 +0200

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-// import { HashRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/react-homework-template">
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
