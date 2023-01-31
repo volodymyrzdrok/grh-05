d17c2c9e47b656e1da5b5cabbef4a7dc756520f1
Volodymyr Zdrok <volodymyrzdrok44@gmail.com>
2023-01-31 19:36:20 +0200

    adddd BrowserRoute

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import { HashRouter } from 'react-router-dom';
+// import { HashRouter } from 'react-router-dom';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-// import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter basename="/grh-05">
     <App />
-  </HashRouter>
+  </BrowserRouter>
 );
